26d9bacd298a453b4753d4bef5bc3477e752f3da
AlexanderB777 <v1997ba@yandex.ru>
2024-12-03 08:31:28 +0300

    Дипломный проект 3 часть

--- ewm-main-service/src/main/java/ru/practicum/controller/priv/PrivateEventsController.java
+++ ewm-main-service/src/main/java/ru/practicum/controller/priv/PrivateEventsController.java
@@ -83,6 +83,7 @@ public class PrivateEventsController {
         return service.putDislike(userId, eventId);
     }
 
+
     @DeleteMapping("/{eventId}/like")
     public void deleteLike(@PathVariable Long userId,
                            @PathVariable Long eventId) {
